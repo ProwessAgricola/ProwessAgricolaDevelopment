c8acaa49a96838fab827d35b360a5692e546fb04
WilliamLeon07 <waleon@espe.edu.ec>
2023-10-27 09:35:37 -0500

    actualizacionV 1.0.2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from 'dotenv';
-import fs from 'fs';
 const app = express();
 app.use(express.json())
 app.use(cors());
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 import multer from 'multer';
 const almacenamiento = multer.memoryStorage();
